c334298ec83507d9ffab359c0037e1fe29ee0ea4
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2020-11-28 12:46:17 +0300

    v 0.0.3

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 import re
+import pytz
 import asyncio
+from time import sleep
 from fs import read_json
 from datetime import datetime
 from telethon import TelegramClient
 from telethon.tl import functions, types
 
 CONFIG = read_json('config/config.json')
+CHANNELS = read_json('config/channels.json')['channels']
+words = read_json('config/search_words.json')['words']
+excluded_words = read_json('config/excluded_words.json')['excluded']
 
+LAST_RUN_DATE = None
 
 client = TelegramClient('my_session', CONFIG['app_id'], CONFIG['app_hash'])
 client.start()
 
 
-CHANNELS = read_json('config/channels.json')['channels']
-words = read_json('config/search_words.json')['words']
-excluded_words = read_json('config/excluded_words.json')['excluded']
-
-
-LAST_MESSAGE_DATE = None  # save it to json, for loading if bot restated
-# import telegram func from snippet and use MARKDOWN format
-# all messages example in jupyer notebook
-
-
 async def main():
+    global LAST_RUN_DATE
+
+    if LAST_RUN_DATE is None:
+        LAST_RUN_DATE = datetime.utcnow()
+
     filtered_msg = []
 
     def format_msg(msg):
@@ -48,15 +49,23 @@ async def main():
                         for ex_w in excluded_words:
                             if w in x.message and ex_w not in x.message:
                                 if last_msg_id == 0 or last_msg_id != x.id:
-                                    filtered_msg.append([x.id, datetime.strftime(
-                                        x.date, '%m-%d-%Y %H:%M:%S'), format_msg(x.text)])
-                                    last_msg_id = x.id
+                                    if x.date.replace(tzinfo=None) >= LAST_RUN_DATE:
+                                        filtered_msg.append([x.id, datetime.strftime(
+                                            x.date, '%m-%d-%Y %H:%M:%S'), format_msg(x.text)])
+                                        last_msg_id = x.id
 
+    LAST_RUN_DATE = datetime.utcnow()
     for fm in filtered_msg:
         print(fm)
 
     print(len(filtered_msg))
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+while True:
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+        sleep(60)
+        print('RUN AGAIN')
+    except Exception as e:
+        print(str(e))
