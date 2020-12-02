00d38f32e29f8191886fd64b7db5178538c4b263
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2020-12-02 20:49:56 +0300

    v 0.1.8

--- app.py
+++ app.py
@@ -5,20 +5,24 @@ from fs import read_json
 from datetime import datetime
 from telethon import TelegramClient
 from telethon.tl import functions, types
-from send_to_telegram_api import telegram_send_message
 
 CONFIG = read_json('config/config.json')
-BOT_API = CONFIG['bot_api_key']
-CHANNEL_CHAT_ID = CONFIG['chat_id']
 
+
+CHANNEL_CHAT_ID = CONFIG['chat_id']
+CHANNEL_CHAT_ID_2 = CONFIG['chat_id_2']
 
 CHANNELS = read_json('config/channels.json')['channels']
 
-WORDS = read_json('config/search_words.json')['words']
-WORDS_EXCLUDED = read_json('config/excluded_words.json')['excluded']
+WORDS_CONFIG = read_json('config/words.json')
 
-WORDS_2 = read_json('config/search_words.json')['words_2']
-WORDS_EXCLUDED_2 = read_json('config/excluded_words.json')['excluded_2']
+GLOBAL_WORDS = WORDS_CONFIG['global_words']
+
+WORDS = WORDS_CONFIG['words']
+WORDS_EXCLUDED = WORDS_CONFIG['excluded']
+
+WORDS_2 = WORDS_CONFIG['words_2']
+WORDS_EXCLUDED_2 = WORDS_CONFIG['excluded_2']
 
 LAST_RUN_DATE = None
 IS_FIRST_RUN = True
@@ -26,61 +30,60 @@ IS_FIRST_RUN = True
 client = TelegramClient('my_session', CONFIG['app_id'], CONFIG['app_hash'])
 client.start()
 
-CHANNELS_ENTITY = []
-
 
 async def main():
     global LAST_RUN_DATE
-    global CHANNELS_ENTITY
     global IS_FIRST_RUN
 
     if LAST_RUN_DATE is None:
         LAST_RUN_DATE = datetime.utcnow()
 
-    filtered_msg = []
+    global_pattern = re.compile('|'.join(GLOBAL_WORDS), re.IGNORECASE)
 
-    def format_msg(msg):
-        # remove copyright
-        msg = re.sub(r"\[(.+)\]\(.+\)", r"", msg)
-        if '\n—————————\n' in msg:
-            msg = re.sub(
-                r"\(?https?://(t.me/inwork_collection|t.me/InWork_team_bot|t.me/inwork_udalenka_bot).+?\)?", r"", msg)
-            msg = msg[:msg.find('\n—————————\n')]
-        return msg
+    include_pattern = re.compile('|'.join(WORDS), re.IGNORECASE)
+    if len(WORDS_EXCLUDED) != 0:
+        exclude_pattern = re.compile('|'.join(WORDS_EXCLUDED), re.IGNORECASE)
 
-    if len(CHANNELS_ENTITY) == 0:
-        for ch in CHANNELS:
-            channel = await client.get_entity(f'@{ch}')
-            CHANNELS_ENTITY.append(channel)
-            await asyncio.sleep(5)
+    include_pattern_2 = re.compile('|'.join(WORDS_2), re.IGNORECASE)
+    if len(WORDS_EXCLUDED_2) != 0:
+        exclude_pattern_2 = re.compile(
+            '|'.join(WORDS_EXCLUDED_2), re.IGNORECASE)
 
-    for c in CHANNELS_ENTITY:
-        await asyncio.sleep(5)
-        messages = await client.get_messages(c, limit=1)
+    for c in CHANNELS:
+        await asyncio.sleep(3)
+        messages = await client.get_messages(c, limit=10)
         last_msg_id = 0
+        last_msg_id_2 = 0
+
         if len(messages) > 0:
-            for x in messages:
-                if x.text is not None:
-                    for w in WORDS:
-                        if len(WORDS_EXCLUDED) > 0:
-                            for ex_w in WORDS_EXCLUDED:
-                                if w in x.message and ex_w not in x.message:
-                                    if last_msg_id == 0 or last_msg_id != x.id:
-                                        if x.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
-                                            filtered_msg.append(
-                                                format_msg(x.text))
-                                            last_msg_id = x.id
-                        else:
-                            if w in x.message:
-                                if last_msg_id == 0 or last_msg_id != x.id:
-                                    if x.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
-                                        filtered_msg.append(format_msg(x.text))
-                                        last_msg_id = x.id
+            for message in messages:
+                if type(message.text) == str:
+                    # First channel
+                    if bool(include_pattern.search(message.text)) and bool(global_pattern.search(message.text)):
+                        if len(WORDS_EXCLUDED) != 0:
+                            if not bool(exclude_pattern.search(message.text)):
+                                if last_msg_id == 0 or last_msg_id != message.id:
+                                    if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
+                                        await client.forward_messages(CHANNEL_CHAT_ID, message, c)
+                        if len(WORDS_EXCLUDED) == 0:
+                            if last_msg_id == 0 or last_msg_id != message.id:
+                                if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
+                                    await client.forward_messages(CHANNEL_CHAT_ID, message, c)
+
+                    # second channel
+                    if bool(include_pattern_2.search(message.text)) and bool(global_pattern.search(message.text)):
+                        if len(WORDS_EXCLUDED_2) != 0:
+                            if not bool(exclude_pattern_2.search(message.text)):
+                                if last_msg_id_2 == 0 or last_msg_id_2 != message.id:
+                                    if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
+                                        await client.forward_messages(CHANNEL_CHAT_ID_2, message, c)
+                        if len(WORDS_EXCLUDED_2) == 0:
+                            if last_msg_id_2 == 0 or last_msg_id_2 != message.id:
+                                if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or IS_FIRST_RUN:
+                                    await client.forward_messages(CHANNEL_CHAT_ID_2, message, c)
 
     LAST_RUN_DATE = datetime.utcnow()
     IS_FIRST_RUN = False
-    for fm in filtered_msg:
-        telegram_send_message(BOT_API, CHANNEL_CHAT_ID, fm)
 
 
 def start():
@@ -88,7 +91,7 @@ def start():
         try:
             loop = asyncio.get_event_loop()
             loop.run_until_complete(main())
-            sleep(600)
+            sleep(10)
         except Exception as e:
             print(str(e))
 
