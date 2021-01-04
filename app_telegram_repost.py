160f7a29bb1234c1104bc86e1990888c8f6a8682
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2021-01-04 21:23:48 +0300

    v 0.3.5

--- app_telegram_repost.py
+++ app_telegram_repost.py
@@ -5,9 +5,9 @@ from time import sleep
 from fs import read_json
 from datetime import datetime
 from spacy.lang.ru import Russian
-from telethon import TelegramClient
 from spacy.matcher import PhraseMatcher
 from telethon.tl import functions, types
+from telethon import TelegramClient, sync, events
 
 
 CONFIG = read_json('config/config.json')
@@ -18,8 +18,6 @@ CHANNEL_2_ENABLED = CONFIG['channel_2_ebabled'].lower()
 
 CHANNEL_CHAT_ID = CONFIG['chat_id']
 CHANNEL_CHAT_ID_2 = CONFIG['chat_id_2']
-
-SLEEP_TIMEOUT = CONFIG['sleep_timeout_seconds']
 
 CHANNELS = read_json('config/channels.json')['channels']
 
@@ -33,9 +31,6 @@ PHRASES_EXCLUDED = PHRASES_CONFIG['excluded']
 PHRASES_2 = PHRASES_CONFIG['phrases_2']
 PHRASES_EXCLUDED_2 = PHRASES_CONFIG['excluded_2']
 
-# FIRST_RUN = True  # If True the Bot will posts a few "initial messages"
-FIRST_RUN = False  # If False you will able to restart the bot "smoothly"
-LAST_RUN_DATE = None
 
 client = TelegramClient('my_session', CONFIG['app_id'], CONFIG['app_hash'])
 client.start()
@@ -56,7 +51,6 @@ if len(PHRASES) > 0:
 
 if ENABLED_FILTERING == 'yes':
     # global_pattern = re.compile('|'.join(GLOBAL_WORDS), re.IGNORECASE)
-
     if CHANNEL_1_ENABLED == 'yes':
         # # include_pattern = re.compile('|'.join(WORDS), re.IGNORECASE)
         # if len(PHRASES_EXCLUDED) != 0:
@@ -74,91 +68,39 @@ if ENABLED_FILTERING == 'yes':
         #             '|'.join(WORDS_EXCLUDED_2), re.IGNORECASE)
 
 
-async def main():
-    global FIRST_RUN
-    global LAST_RUN_DATE
-
-    if LAST_RUN_DATE is None:
-        LAST_RUN_DATE = datetime.utcnow()
-
-    for c in CHANNELS:
-        await asyncio.sleep(5)
-        messages = await client.get_messages(c, limit=20)
-
-        if len(messages) > 0:
-            for message in messages:
-                if type(message.text) == str:
-                    sleep(1)
-                    # First channel
-                    if CHANNEL_1_ENABLED == 'yes':
-                        if ENABLED_FILTERING == 'yes':
-
-                            sentence = nlp(message.text)
-
-                            global_matched_phrases = global_phrase_matcher(
+@client.on(events.NewMessage(chats=CHANNELS))
+async def new_start(event):
+    # First channel
+    if CHANNEL_1_ENABLED == 'yes':
+        if ENABLED_FILTERING == 'yes':
+            sentence = nlp(event.message.text)
+            global_matched_phrases = global_phrase_matcher(
+                sentence)
+            if len(global_matched_phrases) > 0:
+                if len(PHRASES) > 0:
+                    matched_phrases = phrase_matcher(sentence)
+                    if len(matched_phrases) > 0:
+                        if len(PHRASES_EXCLUDED) > 0:
+                            exclude_matched_phrases = exclude_phrase_matcher(
                                 sentence)
+                            if len(exclude_matched_phrases) == 0:
+                                await client.forward_messages(CHANNEL_CHAT_ID, event.message)
 
-                            if len(global_matched_phrases) > 0:
+                        if len(PHRASES_EXCLUDED) == 0:
+                            await client.forward_messages(CHANNEL_CHAT_ID, event.message)
 
-                                if len(PHRASES) > 0:
-                                    matched_phrases = phrase_matcher(sentence)
+                if len(PHRASES) == 0:
+                    if len(PHRASES_EXCLUDED) > 0:
+                        exclude_matched_phrases = exclude_phrase_matcher(
+                            sentence)
+                        if len(exclude_matched_phrases) == 0:
+                            await client.forward_messages(CHANNEL_CHAT_ID, event.message)
+                    if len(PHRASES_EXCLUDED) == 0:
+                        await client.forward_messages(CHANNEL_CHAT_ID, event.message)
 
-                                    if len(matched_phrases) > 0:
-                                        if len(PHRASES_EXCLUDED) > 0:
-                                            exclude_matched_phrases = exclude_phrase_matcher(
-                                                sentence)
-
-                                            if len(exclude_matched_phrases) == 0:
-
-                                                if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                                                    await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-
-                                        if len(PHRASES_EXCLUDED) == 0:
-                                            if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                                                await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-
-                                if len(PHRASES) == 0:
-                                    if len(PHRASES_EXCLUDED) > 0:
-                                        exclude_matched_phrases = exclude_phrase_matcher(
-                                            sentence)
-                                        if len(exclude_matched_phrases) == 0:
-                                            if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                                                await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-                                    if len(PHRASES_EXCLUDED) == 0:
-                                        if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                                            await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-
-                        else:
-                            if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                                await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-
-                    # second channel
-                    # if CHANNEL_2_ENABLED == 'yes':
-                    #     if ENABLED_FILTERING == 'yes':
-                    #         if bool(include_pattern_2.search(message.text)) and bool(global_pattern.search(message.text)):
-                    #             if len(WORDS_EXCLUDED_2) != 0:
-                    #                 if not bool(exclude_pattern_2.search(message.text)):
-                    #                     if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                    #                         await client.forward_messages(CHANNEL_CHAT_ID_2, message, c)
-                    #             if len(WORDS_EXCLUDED_2) == 0:
-                    #                 if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                    #                     await client.forward_messages(CHANNEL_CHAT_ID_2, message, c)
-                    #     else:
-                    #         if message.date.replace(tzinfo=None) >= LAST_RUN_DATE or FIRST_RUN:
-                    #             await client.forward_messages(CHANNEL_CHAT_ID, message, c)
-    FIRST_RUN = False
-    LAST_RUN_DATE = datetime.utcnow()
-
-
-def start():
-    while True:
-        try:
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(main())
-            sleep(SLEEP_TIMEOUT)
-        except Exception as e:
-            print(str(e))
+        else:
+            await client.forward_messages(CHANNEL_CHAT_ID, event.message)
 
 
 if __name__ == "__main__":
-    start()
+    client.run_until_disconnected()
