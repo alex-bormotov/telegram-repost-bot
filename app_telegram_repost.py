c2d444dd2d8e42ddbb23860993362c98d62dbba5
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2021-01-13 15:22:39 +0300

    v 0.5.5

--- app_telegram_repost.py
+++ app_telegram_repost.py
@@ -12,16 +12,14 @@ from telethon import TelegramClient, sync, events
 
 CONFIG = read_json('config/config.json')
 CHANNELS = read_json('config/channels.json')['channels']
-PHRASES_CONFIG = read_json('config/phrases.json')
+WORDS_CONFIG = read_json('config/words.json')
 
 CHANNEL_1_ENABLED_FILTERING = CONFIG['channel_1_enabled_filtering'].lower()
 CHANNEL_CHAT_ID_1 = CONFIG['chat_id_1']
 
-CHANNEL_1_PHRASES_1 = PHRASES_CONFIG['channel_1_phrases_1']
-CHANNEL_1_PHRASES_2 = PHRASES_CONFIG['channel_1_phrases_2']
-CHANNEL_1_PHRASES_2_1 = PHRASES_CONFIG['channel_1_phrases_2_1']
-CHANNEL_1_PHRASES_3 = PHRASES_CONFIG['channel_1_phrases_3']
-CHANNEL_1_PHRASES_3_1 = PHRASES_CONFIG['channel_1_phrases_3_1']
+WORDS_1 = WORDS_CONFIG['words_1']
+WORDS_2 = WORDS_CONFIG['words_2']
+WORDS_2_1 = WORDS_CONFIG['words_2_1']
 
 
 client = TelegramClient('my_session', CONFIG['app_id'], CONFIG['app_hash'])
@@ -30,56 +28,48 @@ client.start()
 nlp = Russian()
 
 
-channel_1_phrase_matcher_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
-channel_1_patterns_1 = [nlp(text_phase_1)
-                        for text_phase_1 in CHANNEL_1_PHRASES_1]
-channel_1_phrase_matcher_1.add('AI_INC_1', None, *channel_1_patterns_1)
+matcher_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
+matcher_1.add('AI_INC_1', None, *[nlp(text_phase_1)
+                                  for text_phase_1 in WORDS_1])
 
+matcher_2 = PhraseMatcher(nlp.vocab, attr='LOWER')
+matcher_2.add('AI_INC_2', None, *[nlp(text_phase_2)
+                                  for text_phase_2 in WORDS_2])
 
-channel_1_phrase_matcher_2 = PhraseMatcher(nlp.vocab, attr='LOWER')
-channel_1_patterns_2 = [nlp(text_phase_2)
-                        for text_phase_2 in CHANNEL_1_PHRASES_2]
-channel_1_phrase_matcher_2.add('AI_INC_2', None, *channel_1_patterns_2)
-
-
-channel_1_phrase_matcher_2_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
-channel_1_patterns_2_1 = [nlp(text_phase_2_1)
-                          for text_phase_2_1 in CHANNEL_1_PHRASES_2_1]
-channel_1_phrase_matcher_2_1.add('AI_INC_2_1', None, *channel_1_patterns_2_1)
-
-
-channel_1_phrase_matcher_3 = PhraseMatcher(nlp.vocab, attr='LOWER')
-channel_1_patterns_3 = [nlp(text_phase_3)
-                        for text_phase_3 in CHANNEL_1_PHRASES_3]
-channel_1_phrase_matcher_3.add('AI_INC_4', None, *channel_1_patterns_3)
-
-
-channel_1_phrase_matcher_3_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
-channel_1_patterns_3_1 = [nlp(text_phase_3_1)
-                          for text_phase_3_1 in CHANNEL_1_PHRASES_3_1]
-channel_1_phrase_matcher_3_1.add(
-    'AI_INC_3_1', None, *channel_1_patterns_3_1)
+matcher_2_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
+matcher_2_1.add('AI_INC_2_1', None, *
+                [nlp(text_phase_2_1) for text_phase_2_1 in WORDS_2_1])
 
 
 @client.on(events.NewMessage(chats=CHANNELS))
 async def new_start(event):
+    try:
+        # print(event.message.text)
 
-    if CHANNEL_1_ENABLED_FILTERING == 'yes':
+        if CHANNEL_1_ENABLED_FILTERING == 'yes':
 
-        sentence_1 = nlp(event.message.text)
+            sentence_1 = nlp(event.message.text)
 
-        matched_phrases_1 = channel_1_phrase_matcher_1(sentence_1)
-        matched_phrases_2 = channel_1_phrase_matcher_2(sentence_1)
-        matched_phrases_3 = channel_1_phrase_matcher_3(sentence_1)
-        matched_phrases_4 = channel_1_phrase_matcher_4(sentence_1)
-        matched_phrases_4_1 = channel_1_phrase_matcher_4_1(sentence_1)
-        if len(matched_phrases_1) != 0:
-            if (len(matched_phrases_2) != 0 and len(matched_phrases_3) != 0) or (len(matched_phrases_4) != 0 and len(matched_phrases_4_1) != 0):
+            matched_1 = matcher_1(sentence_1)
+            matched_2 = matcher_2(sentence_1)
+            matched_2_1 = matcher_2_1(sentence_1)
+
+            condition_1 = len(matched_1) != 0
+            condition_2 = len(matched_2) != 0 and len(matched_2_1) != 0
+
+            print(len(matched_1), len(matched_2), len(matched_2_1))
+            print(condition_1, condition_2)
+
+            if condition_1 or condition_2:
+
+                print(event.message.text)
+
                 await client.forward_messages(CHANNEL_CHAT_ID_1, event.message)
 
-    else:
-        await client.forward_messages(CHANNEL_CHAT_ID_1, event.message)
-
+        else:
+            await client.forward_messages(CHANNEL_CHAT_ID_1, event.message)
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     client.run_until_disconnected()
