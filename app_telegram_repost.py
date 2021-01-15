8a9c788384e26697f704519ddf6c5532c16cd022
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2021-01-15 17:52:29 +0300

    v 0.5.9

--- app_telegram_repost.py
+++ app_telegram_repost.py
@@ -20,6 +20,7 @@ CHANNEL_CHAT_ID_1 = CONFIG['chat_id_1']
 WORDS_1 = WORDS_CONFIG['words_1']
 WORDS_2 = WORDS_CONFIG['words_2']
 WORDS_2_1 = WORDS_CONFIG['words_2_1']
+WORDS_2_0 = WORDS_CONFIG['words_2_0']
 
 
 client = TelegramClient('my_session', CONFIG['app_id'], CONFIG['app_hash'])
@@ -40,6 +41,10 @@ matcher_2_1 = PhraseMatcher(nlp.vocab, attr='LOWER')
 matcher_2_1.add('AI_INC_2_1', None, *
                 [nlp(text_phase_2_1) for text_phase_2_1 in WORDS_2_1])
 
+matcher_2_0 = PhraseMatcher(nlp.vocab, attr='LOWER')
+matcher_2_0.add('AI_INC_2_0', None, *
+                [nlp(text_phase_2_0) for text_phase_2_0 in WORDS_2_0])
+
 
 @client.on(events.NewMessage(chats=CHANNELS))
 async def new_start(event):
@@ -48,18 +53,16 @@ async def new_start(event):
         if CHANNEL_1_ENABLED_FILTERING == 'yes':
             sentence_1 = nlp(event.message.text)
 
-            matched_1 = matcher_1(sentence_1)
-            matched_2 = matcher_2(sentence_1)
-            matched_2_1 = matcher_2_1(sentence_1)
+            matched_1 = matcher_1(sentence_1)  # include
+            matched_2 = matcher_2(sentence_1)  # include
+            matched_2_1 = matcher_2_1(sentence_1)  # include
+            matched_2_0 = matcher_2_0(sentence_1)  # exclude
 
             condition_1 = len(matched_1) != 0
             condition_2 = len(matched_2) != 0 and len(matched_2_1) != 0
+            condition_3 = len(matched_2_0) == 0
 
-            # print(len(matched_1), len(matched_2), len(matched_2_1))
-            # print(condition_1, condition_2)
-
-            if condition_1 or condition_2:
-                # print(event.message.text)
+            if (condition_1 or condition_2) and condition_3:
                 await client.forward_messages(CHANNEL_CHAT_ID_1, event.message)
 
         else:
--- delete_all_channel_msg.py
+++
@@ -1,6 +0,0 @@
-
-
-bot.delete_message(chat_id=message.chat_id,
-                   message_id=message.message_id,
-                   *args,
-                   **kwargs)
